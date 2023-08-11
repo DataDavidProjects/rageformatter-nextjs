b7aeb0506cf508f67545084d09268eb7484691cd
Davide Lupis <davide_lupis@yahoo.it>
2023-08-11 09:26:10 +0200

    tutorial page: fixing layout

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -4,16 +4,18 @@ import Image from "next/image";
 
 const TutorialPage = () => {
   return (
-    <div className="relative flex min-h-screen m-6 text-sm items-center justify-center py-12 px-4">
+    <div className="relative flex  m-6 text-sm items-center justify-center py-12 px-4">
       <div className="max-w-3xl bg-white p-10 rounded-lg shadow-xl w-full text-gray-900">
-        <h2 className="text-gray-900 text-4xl mb-10 font-semibold tracking-tighter text-center">
+        <h2 className="text-gray-900 text-xl mb-5 font-semibold tracking-tighter sm:text-center sm:text-2xl">
           Corporate Guidelines 🧑‍💼
         </h2>
-
+        <p>
+          This simple steps will turn your rage into a promotion in no time.
+        </p>
         <div className="bg-red-50 p-8 rounded-lg border border-red-200 mb-10">
           <ul className="list-decimal pl-5 space-y-6">
             <li>
-              <span className="mb-2 font-semibold block">
+              <span className="mb-2 font-semibold block text-xs">
                 Write your frustrated message in the card form provided.
               </span>
               <div className="relative w-70% h-70%  mx-auto mt-4 rounded-md overflow-hidden shadow-md border border-gray-200 sm:w-80 h-90">
@@ -27,11 +29,11 @@ const TutorialPage = () => {
                 />
               </div>
             </li>
-            <li className="font-semibold">
+            <li className="font-semibold text-xs">
               Click on the &quot;Format&quot; button.
             </li>
 
-            <li className="font-semibold">
+            <li className="font-semibold text-xs">
               A new card will appear below with a clean, polite version of your
               message.
               <div className="relative w-70% h-70% mx-auto mt-4 rounded-md overflow-hidden shadow-md border border-gray-200 sm:w-80 h-90">
@@ -46,15 +48,14 @@ const TutorialPage = () => {
               </div>
             </li>
           </ul>
-          <p className="text-lg font-medium text-center mt-8">
+          <p className="text-xs sm:text-sm font-medium sm:text-center mt-8">
             🚀 Now, send your message with confidence!
           </p>
-          <p className=" text-gray-600 hover:text-red-700 underline transition ease-in-out duration-150 text-center">
+          <p className=" text-gray-600 hover:text-red-700 underline transition ease-in-out duration-150 sm:text-center text-xs">
             ⚠️ Note: Always double check the answer, we are trying to optimize
             the model.
           </p>
         </div>
-
         <div className="text-center">
           <Link href="/">
             <span className="cursor-pointer text-red-600 hover:text-red-700 underline transition ease-in-out duration-150">
