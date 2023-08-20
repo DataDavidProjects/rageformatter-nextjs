2586e221735734e72fc04b056a71d94ee8567de2
Davide Lupis <48164716+DataDavidProjects@users.noreply.github.com>
2023-08-20 13:38:10 +0200

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,24 +1,45 @@
 import React from "react";
 import Link from "next/link";
 import { VscGithub } from "react-icons/vsc";
+import { FaInstagram } from "react-icons/fa";
+import { AiOutlineLinkedin } from "react-icons/ai";
 
 const Footer = () => {
   const current_year = new Date().getFullYear();
   return (
-    <footer className="bg-gradient-to-b from-red-700 to-red-900 text-white py-6 mt-auto flex flex-col items-center space-y-4 ">
+    <footer className="bg-gradient-to-b from-red-700 to-red-900 text-white text-sm py-6 mt-auto flex flex-col items-center space-y-4">
       <p>🇮🇹 RageFormatter © {current_year} - All right reserved🍝</p>
-      <p className="text-sm">
+      <p className="text-xs">
         Supported Languages:
-        <span> 🇮🇹 🇬🇧</span>
+        <span className="ml-1"> 🇮🇹 🇬🇧</span>
       </p>
       <Link
-        href="https://github.com/DataDavidProjects/rageformatter-nextjs#-rageformatter"
-        className="flex items-center space-x-2 text-white font-semibold"
+        href={"/privacy-policy"}
+        className="text-white font-semibold text-xs"
       >
-        <VscGithub size={24} />
-        <span>Davide Lupis</span>
+        Privacy Policy
       </Link>
-      <Link href={"/privacy-policy"}> Privacy Policy</Link>
+      <p className="text-xs text-center">Built by Davide Lupis:</p>
+      <div className="flex items-center space-x-2">
+        <Link
+          href="https://github.com/DataDavidProjects/rageformatter-nextjs#-rageformatter"
+          className="flex items-center space-x-2 text-white font-semibold"
+        >
+          <VscGithub size={22} />
+        </Link>
+        <Link
+          href="https://www.instagram.com/david_zoolander/"
+          className="flex items-center space-x-2 text-white font-semibold"
+        >
+          <FaInstagram size={22} />
+        </Link>
+        <Link
+          href="https://it.linkedin.com/in/davide-lupis-448578153"
+          className="flex items-center space-x-2 text-white font-semibold"
+        >
+          <AiOutlineLinkedin size={22} />
+        </Link>
+      </div>
     </footer>
   );
 };
